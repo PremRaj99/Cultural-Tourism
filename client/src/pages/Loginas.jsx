26b4f2be8484d99eb7753dde05453736cab578ea
Rohit Kumar <rohitkumar.devexpert@gmail.com>
2024-04-06 14:11:10 +0530

    login page again updated

--- client/src/pages/Loginas.jsx
+++ client/src/pages/Loginas.jsx
@@ -49,7 +49,7 @@ export default function Loginas() {
 
     <>
 
-    <img src={bg}
+    <img src="https://images.unsplash.com/photo-1576094169187-1b006b6a8d57?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
      alt=""  
      className="w-full relative"/>
 
