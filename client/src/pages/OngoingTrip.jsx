afe320350b27526eac9cebc22c17150ea49ceed7
Rohit Kumar <rohitkumar.devexpert@gmail.com>
2024-04-06 12:51:22 +0530

    Ongoing Trip

--- client/src/pages/OngoingTrip.jsx
+++ client/src/pages/OngoingTrip.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function OngoingTrip() {
   return (
-    <div>OngoingTrip</div>
+    <div className='flex flex-col mx-auto gap-34px'>
+
+      <h1 className='font-bold flex justify-center my-5 '>Ongoing Trip</h1>
+     
+     <div className='flex justify-around align-center gap gap-7 '>
+
+      <div  >
+      <h3 className='font-bold my-2'>Trip Name</h3>
+      <h4>Goa WeekEnd</h4>
+      </div>
+
+      <div>   
+      <h3 className='font-bold my-2'>Trip Status</h3>
+      <h4>Active</h4>
+      </div>
+      
+      <div>
+      <h3 className='font-bold my-2'>Trip Package Price</h3>
+      <h4>Rs. 22000</h4>
+      </div>
+
+     </div>
+    </div>
   )
 }
